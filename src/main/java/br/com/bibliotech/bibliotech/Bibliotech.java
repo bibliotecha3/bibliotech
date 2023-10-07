e5bf9a18c41566841efda1b8ca7d68727ed4b288
Bibliotech <bibliotech.a3@gmail.com>
2023-10-07 13:34:22 -0300

    Entrega01(gui): GUI de login sem funcionalidades

---
+++ src/main/java/br/com/bibliotech/bibliotech/Bibliotech.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.bibliotech.bibliotech;
+
+/**
+ *
+ * @author unity
+ */
+public class Bibliotech {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
