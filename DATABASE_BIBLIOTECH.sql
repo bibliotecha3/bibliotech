33b413c1849acb3edaa809f93840a53f6c2af861
bibliotecha3 <146670592+bibliotecha3@users.noreply.github.com>
2023-10-13 17:08:30 -0300

    Entrega02(bd): Script do banco de dados

---
+++ DATABASE_BIBLIOTECH.sql
@@ -0,0 +1,18 @@
+CREATE database users_bibliotech;
+USE users_bibliotech;
+-- DROP DATABASE users_bibliotech;
+CREATE TABLE users_bibliotech(
+	cod_pessoa INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(100),
+	telefone VARCHAR(20),
+	email VARCHAR(100),
+    cidade VARCHAR(50)
+    );
+-- DROP TABLE users_bibliotech;
+CREATE TABLE livros(
+id_livros INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(100),
+autor INT(10),
+genero VARCHAR(20),
+paginas DECIMAL(1000,5)
+);
