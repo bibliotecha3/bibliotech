09d7db709038613ed2ce0ad001e43267cbbd09c5
bibliotecha3 <146670592+bibliotecha3@users.noreply.github.com>
2023-10-29 18:12:49 -0300

    my sql error WMI

--- DATABASE_BIBLIOTECH.sql
+++ DATABASE_BIBLIOTECH.sql
@@ -4,6 +4,7 @@ USE users_bibliotech;
 CREATE TABLE users_bibliotech(
 	cod_pessoa INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(100),
+	senha VARCHAR(100),
 	telefone VARCHAR(20),
 	email VARCHAR(100),
     cidade VARCHAR(50)
